187a6bd91745b50755aea91397a0e8b40fc893bc
ItzSylex <brandon162000@gmail.com>
2020-10-18 17:55:39 -0600

    Working comprar, falta add_item

--- cogs/economy.py
+++ cogs/economy.py
@@ -3,11 +3,16 @@ from discord.ext import commands, tasks
 
 import constants
 import config
+import ast
 
 import econstants
 import random
 import datetime
 
+
+## TODO: Falta por terminar el comando de comprar, testear que la resta del dinero se haga bien con todos los casos
+## esto es, cuando tiene dinero suficiente solo en la wallet y cuando no, en este caso necesita del banco.
+# La wallet deberia de quedar en 0 y el banco con el remaining.
 
 class Economia(commands.Cog):
 
@@ -20,7 +25,6 @@ class Economia(commands.Cog):
     @tasks.loop(seconds = 2)
     async def update_shop(self):
         result = datetime.datetime.now() - self.last_time_shop
-        print(result.total_seconds())
         if result.total_seconds() >= 14400:
             self.shop = random.sample(econstants.shop, 6)
             self.last_time_shop = datetime.datetime.now()
@@ -45,6 +49,11 @@ class Economia(commands.Cog):
 
             sql = """UPDATE economy SET wallet = wallet - ? WHERE user_id = ?"""
             data_tuple = (amount, member.id)
+
+        if tipo == "update":
+            sql = """UPDATE economy SET wallet = ?, bank = ? WHERE user_id = ?"""
+            data_tuple = (amount[0], amount[1], member.id)
+
 
         await self.bot.db.execute(sql, data_tuple)
         await self.bot.db.commit()
@@ -95,6 +104,38 @@ class Economia(commands.Cog):
 
         return data[0]
 
+    async def add_item(self, member, item):
+        inventory = await self.get_inventory(member)
+        converted_inventory = ast.literal_eval(inventory)
+
+        if not converted_inventory:
+            to_add = [{item['name'] : [1, item['emoji'], item['description']]}]
+            data_tuple = (str(to_add), member.id)
+
+        else:
+            for each in converted_inventory:
+                print(type(each))
+                if item['name'] in each.keys():
+
+                    detalles = each[item["name"]]
+                    detalles[0] = detalles[0] + 1
+                    data_tuple = (str(converted_inventory), member.id)
+
+                else:
+
+                    count = 1
+                    to_add = {item['name'] : [count, item['emoji'], item['description']]}
+
+                    converted_inventory.append(to_add)
+
+                    data_tuple = (str(converted_inventory), member.id)
+
+        sql = """UPDATE economy SET inventory = ? WHERE user_id = ?"""
+
+        await self.bot.db.execute(sql, data_tuple)
+        await self.bot.db.commit()
+
+
     @commands.command(
         brief = "Deposita tus quacks al banco",
         usage = f"{config.prefix}depositar [cantidad]"
@@ -102,7 +143,6 @@ class Economia(commands.Cog):
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def depositar(self, ctx, amount):
         result = await self.transaction(ctx.author, amount, "to_bank")
-        print(result)
         embed = discord.Embed(
             color = constants.blue
         )
@@ -196,7 +236,7 @@ class Economia(commands.Cog):
     @commands.cooldown(1, 180, commands.BucketType.user)
     async def buscar(self, ctx):
         rand_amount = random.randint(50, 100)
-        chance = random.choices([rand_amount, 0, 1], weights = [.7, .3, .01])
+        chance = random.choices([rand_amount, 0, 1], weights = [.6, .4, .01])
 
         embed = discord.Embed(
             color = constants.blue
@@ -220,17 +260,30 @@ class Economia(commands.Cog):
         usage = f"{config.prefix}inventario"
     )
     @commands.cooldown(1, 1, commands.BucketType.user)
-    async def inventario(self, ctx, member: discord.Member = None):
-        print(await self.get_inventory(ctx.author))
+    async def inventario(self, ctx):
+        embed = discord.Embed(
+            title = f"{constants.check} Inventario de {ctx.author}",
+            color = constants.blue
+        )
+        inventory = await self.get_inventory(ctx.author)
+        converted_inventory = ast.literal_eval(inventory)
+
+        format_string = ""
+
+        for item in converted_inventory:
+            format_string = format_string + f"{item['emoji']} **{item['name']}:** ► {item['description']}"
+
+        embed.description = "Parece que no hay nada" if len(format_string) == 0 else format_string
+        await ctx.send(embed = embed)
 
     @commands.command(
         brief = "Muestra la tienda actual, cambia cada 4 horas",
         usage = f"{config.prefix}tienda"
     )
     @commands.cooldown(1, 1, commands.BucketType.user)
-    async def tienda(self, ctx, specific = None):
+    async def tienda(self, ctx, *, specific = None):
 
-        if specific is not None:
+        if not specific:
 
             shop = ""
             embed = discord.Embed(
@@ -242,17 +295,62 @@ class Economia(commands.Cog):
             embed.description = shop
             return await ctx.send(embed = embed)
 
-        for item in self.shop:
-            if item["name"].lower() == specific.lower():
-                embed = discord.Embed(
-                    title = item["name"],
-                    description = f"{item["description"]}\n\n**Precio:** {item["price"]}",
-                    color = constants.blue
-                )
-                embed.set_footer(text = "Puedes usar el comando `comprar` si te interesa.")
-                file = discord.File(f"emotes/{item["file_code"]}.png", filename="image.png")
-                embed.set_thumbnail(url="attachment://image.png")
-                await ctx.send(file=file, embed=embed)
+        else:
+            for item in self.shop:
+                if item["name"].lower() == specific.lower():
+                    embed = discord.Embed(
+                        title = item["name"],
+                        description = f"{item['description']}\n\n**Precio:** {item['price']}",
+                        color = constants.blue
+                    )
+                    embed.set_footer(text = "Puedes usar el comando `comprar` si te interesa.")
+                    file = discord.File(f"emotes/shop/{item['file_code']}.png", filename="image.png")
+                    embed.set_thumbnail(url="attachment://image.png")
+                    await ctx.send(file=file, embed=embed)
+
+    @commands.command(
+        brief = "Compra un item de la tienda.",
+        usage = f"{config.prefix}comprar [item]"
+    )
+    @commands.cooldown(1, 2, commands.BucketType.user)
+    async def comprar(self, ctx, *, item):
+
+        embed = discord.Embed(
+            color = constants.red
+        )
+
+        for current_item in self.shop:
+            if item.lower() == current_item["name"].lower():
+                balance = await self.get_balance(ctx.author)
+
+                total = balance[0] + balance[1]
+                price = current_item["price"]
+
+                if total >= price:
+                    after_wallet = balance[0] - price
+
+                    if after_wallet < 0:
+                        after_bank = balance[1] - (after_wallet * -1)
+                        after_wallet = 0
+                        amount_tuple = (after_wallet, after_bank)
+
+                    else:
+                        amount_tuple = (after_wallet, balance[1])
+
+                    await self.add_item(ctx.author, current_item)
+                    await self.update_amount(ctx.author, amount_tuple, "update")
+
+                    embed.description = f"{constants.check} Has comprado {current_item['name']} correctamente"
+                    embed.color = constants.green
+                    return await ctx.send(embed = embed)
+
+                embed.description = f"{constants.x} Parece que no tienes suficientes quacks para comprar este item."
+                embed.color = constants.red
+                return await ctx.send(embed = embed)
+
+        embed.description = f"{constants.x} Parece que este item no esta en la tienda. Asegurate de escribirlo **igual** a como sale en la tienda."
+        return await ctx.send(embed = embed)
+
 
 
     @commands.Cog.listener()
@@ -296,6 +394,19 @@ class Economia(commands.Cog):
                 return
             await ctx.send(f"Mas lento, intenta de nuevo dentro de {error.retry_after:.2f} segundos")
 
+        if isinstance(error, commands.MissingRequiredArgument):
+            if error.param.name == "amount":
+                embed = discord.Embed(
+                    description = f"{constants.x} Tienes que especificar un monto. Usa `help [comando]` para ver mas informacion",
+                    color = constants.red
+                )
+                await ctx.send(embed = embed)
+            if error.param.name == "item":
+                embed = discord.Embed(
+                    description = f"{constants.x} Especifica el item, asegurate de escribirlo tal y como aparece en la tienda o en tu inventario.",
+                    color = constants.red
+                )
+                await ctx.send(embed =  embed)
         else:
             raise error
 
