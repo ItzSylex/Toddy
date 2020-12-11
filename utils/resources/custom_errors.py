049cf9b35b549a69791f23da0d6fb4e4f444a93b
ItzSylex <brandon162000@gmail.com>
2020-12-10 19:53:41 -0600

    No items error

--- utils/resources/custom_errors.py
+++ utils/resources/custom_errors.py
@@ -4,3 +4,8 @@ from discord.ext import commands
 class NoRequiredRole(commands.DisabledCommand):
     def __init__(self):
         super().__init__("No tienes los roles necesarios para usar este comando")
+
+
+class NoSupplies(commands.DisabledCommand):
+    def __init__(self):
+        super().__init__("No tienes los materiales suficientes para realizar esta accion")
